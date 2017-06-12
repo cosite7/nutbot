17839b4c677f313287aa18424a588181cea4f3fe
cosite7 <cosite7@gmail.com>
2017-06-12 17:18:26 +0700

    up
    up

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'br7TPh8c2j5ANWaUnR7a7CJqHPdE6hIyTh/ArxXw4OMOPuanJ+bF2bnP3AlHJrMZtQZ3Bmm3BbX0WcH31v4OZ8veejHP5YxZ3Z2PVJ+Tl1FqXvhe3y0J9079j/njIjrp6qikBPA8FZ838ZLtDOydtwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
